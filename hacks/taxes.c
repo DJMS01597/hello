98e672c31173df238e74cce2678af6f91201a731
Joseph Flodman <jflodman3@huskers.unl.edu>
2024-09-11 15:49:10 +0000

    Wed, Sep 11, 2024, 10:49 AM -05:00

--- hacks/taxes.c
+++ hacks/taxes.c
@@ -9,44 +9,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
+int main(int argc, char **argv) {
 
-double agi;
-double tax;
-double numChildren;
-double reducedTax;
+  double agi = 0.0;
+  char c = 'N';
+  double tax = 0.0;
+  double childCredit = 0.0;
+  double totalTax = 0.0;
+  int numChildren = 0;
 
-printf("Enter your Adjusted Gross Income:  ");
-scanf("%lf", &agi);
+  printf("Please enter your adjusted gross income (AGI): ");
+  scanf("%lf", &agi);
+  //remove the "enter" endline character
+  getchar();
 
-printf("Enter the number of children:  ");
-scanf("%lf", &numChildren);
+  printf("Do you have any children? (Y) or (N)? ");
+  c = getchar();
 
-if(agi <= 19,900){
-tax = 0.10 * agi;
+  if(c == 'y' || c == 'Y') {
+    printf("How many children do you have? ");
+    scanf("%d", &numChildren);
+  }
 
-}else if(agi <= 81,050){
- tax = 1990 + 0.12 * (agi - 19900);
-}else if(agi <= 172750){
- tax = 9328 + 0.22 * (agi - 81050);
-}else if(agi <= 329850){
-tax = 29502 + 0.24 * (agi - 172750);
-}else if(agi <= 418850){
-tax = 67206 + 0.32 * (agi - 329850);
-}else if(agi <= 628,300){
-tax = 95686 + 0.35 * (agi - 418850);
+  //TODO: compute the tax, child credit, and total tax here
+
+  printf("AGI:          $%10.2f\n", agi);
+  printf("Tax:          $%10.2f\n", tax);
+  printf("Child Credit: $%10.2f\n", childCredit);
+  printf("Total Tax:    $%10.2f\n", totalTax);
+
+  return 0;
 }
-
-reducedTax = (numChildren * 2000) - tax;
-
-  if(tax < 0){
-  tax = 0;
-}
-
-printf("Your total tax is: $%.2f\n", tax);
-
-return 0;
-
-
-}
-

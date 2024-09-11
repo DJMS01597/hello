f8596694c8a252269f2c8f5d93287def0bfcb435
Joseph Flodman <jflodman3@huskers.unl.edu>
2024-09-11 01:13:29 +0000

    Tue, Sep 10, 2024, 8:13 PM -05:00

---
+++ labs/taxes.c
@@ -0,0 +1,45 @@
+/**
+ * Author: Joseph Flodman
+ *
+ * This program prompts the user's total tax based on the user's
+ * AGI the number of children they have.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+
+double agi;
+double numberOfKids;
+double taxTotal;
+
+x = numberOfKids;
+y = agi;
+
+printf("Please type the number of kids your household has: \n");
+scanf("%lf",&numberOfKids);
+
+printf("Please type your agi: \n")
+scanf("%lf", &agi);
+
+
+if((x >= 0) && !(x > 19,900 )) {
+taxTotal = 0 * numberOfKids;
+printf("%f, taxTotal")
+
+;
+
+
+} else if(x == 0) {
+  printf("x is zero, its square root is zero\n");
+
+
+
+} else if(x == 0) {
+  printf("x is zero, its square root is zero\n");
+} else if(x == 0) {
+  printf("x is zero, its square root is zero\n");
+} else if(x == 0) {
+  printf("x is zero, its square root is zero\n");
+}

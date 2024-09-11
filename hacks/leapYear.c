221f6d2a1303b913ab477073e4710289b4d40504
Joseph Flodman <jflodman3@huskers.unl.edu>
2024-09-11 15:14:07 +0000

    Wed, Sep 11, 2024, 10:14 AM -05:00

--- hacks/leapYear.c
+++ hacks/leapYear.c
@@ -7,12 +7,21 @@
 #include <math.h>
 #include<stdlib.h>
 
-int main()
+int main(){
 
-int year;
+double year;
 int testYears() = (2000, 1900, 2004, 2001, 2100);
 double isLeapYear;
-if int(int i = 0; i < 5; ){
-year = testYears(i)
-}if else(isLeapYear)
+if(testYears = 0; i < 5; ){
+year = testYears
+}if else(isLeapYear){
+printf("%d is a leap year.\n", year);
+} else({printf("%d is not a leap year.\n", year)) ;
+}
+
+ return 0;
+
+}
+
+
 
